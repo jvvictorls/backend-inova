3cc95ac5c53cd40685377fe12dcd439e287c3bdc
João Victor <jvvictorls@hotmail.com>
2024-06-20 19:06:24 -0300

    Mudei o banco de dados

--- src/app.js
+++ src/app.js
@@ -1,10 +1,28 @@
 const express = require('express');
-const routes = require('./routes'); 
+const postgres = require('postgres');
+require('dotenv').config();
+console.log(process.env);
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
+const sql = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: 'require',
+});
+
+async function getPgVersion() {
+  const result = await sql`select version()`;
+  console.log(result);
+}
+
+getPgVersion();
 
 const app = express();
 
 app.use(express.json());
 
-app.use(routes);
 
 module.exports = app;
--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express').Router;
-const UsersRouter = require('./Sigin/users.routes');
-
-const router = Router();
-
-
-router.use('/', UsersRouter);
-
-module.exports = router;
