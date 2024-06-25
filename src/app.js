fc7d260a7a4afd575bf985ff3fccc7d659df0bbb
João Victor <jvvictorls@hotmail.com>
2024-06-25 18:57:56 -0300

    FIx: mudei as configurações de CORS

--- src/app.js
+++ src/app.js
@@ -4,9 +4,16 @@ const routes = require('./routes');
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: ['https://teste-omhy.vercel.app', 'http://127.0.0.1:5500'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  optionsSuccessStatus: 200
+};
 
-app.use(express.json()); //para poder a aplicação receber dados via fornmulário.
+app.use(cors(corsOptions));
+
+app.use(express.json()); // Para permitir que a aplicação receba dados via formulário
 
 app.use(routes);
 
