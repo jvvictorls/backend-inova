433adeba16f1f2733ab9eec10e0e489b5322f9ea
João Victor <jvvictorls@hotmail.com>
2024-06-25 17:36:26 -0300

    Fix: Ainda tentando arrumar o cors 2

--- src/app.js
+++ src/app.js
@@ -4,10 +4,7 @@ const routes = require('./routes');
 
 const app = express();
 
-app.use(cors({
-  origin: 'https://teste-omhy.vercel.app/',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}));
+app.use(cors());
 
 app.use(express.json()); //para poder a aplicação receber dados via fornmulário.
 
